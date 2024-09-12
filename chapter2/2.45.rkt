def88b0a56e65bc88397b5e47176a943df06fffe
dempseyt <dempsey.thompson@journeyone.com.au>
2024-09-12 16:55:44 +0800

    Exercise 2.45

--- chapter2/2.45.rkt
+++ chapter2/2.45.rkt
@@ -1,6 +1,7 @@
-#lang sicp
+#lang racket
 
-(#%require (planet "sicp.ss" ("soegaard" "sicp.plt" 2 1)))
+(require sicp-pict)
+(require "./paint-utils.rkt")
 
 (define (split direction1 direction2)
     (lambda (painter n)
@@ -14,21 +15,5 @@
 
 (paint (right-split einstein 2))
 (paint (up-split einstein 2))
-
-(define (corner-split painter n)
-   (if (= n 0)
-      painter
-       (let ((up (up-split painter (- n 1)))
-            (right (right-split painter (- n 1))))
-         (let ((top-left (beside up up))
-               (bottom-right (below right right))
-               (corner (corner-split painter (- n 1))))
-           (beside (below painter top-left)
-                   (below bottom-right corner))))))
-
-(define (square-limit painter n)
-   (let ((quarter (corner-split painter n)))
-     (let ((half (beside (flip-horiz quarter) quarter)))
-      (below (flip-vert half) half))))
-
-(paint (square-limit einstein 2))
+(paint-to-png (right-split einstein 2) "chapter2/2.45a.png")
+(paint-to-png (up-split einstein 2) "chapter2/2.45b.png")
