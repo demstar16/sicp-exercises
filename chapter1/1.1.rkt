91b6e7c47cb524a294b170109c05191408744d42
dempseyt <dempsey.thompson@journeyone.com.au>
2024-09-20 10:35:01 +0800

    Exercise 1.1

--- chapter1/1.1.rkt
+++ chapter1/1.1.rkt
@@ -1,27 +1,23 @@
 #lang racket
 
+(require "../test-harness.rkt")
+
 (define a 3) ; 3
 (define b (+ a 1)) ; 4
 
-(+ a b (* a b)) ; (+ 3 4 (* 3 4)) = 19
-(= a b) ; #f
-
-(if (and (> b a) (< b (* a b))) ; (4 > 3) & (4 < 12)
+(test "Addition" (+ a b (* a b)) 19)
+(test "Equality" (= a b) #f)
+(test "If" (if (and (> b a) (< b (* a b))) ; (4 > 3) & (4 < 12)
     b
-    a) 
-; 4
-
-(cond ((= a 4) 6)
+    a) 4)
+(test "Cond" (cond ((= a 4) 6)
         ((= b 4) (+ 6 7 a))
-        (else 25))
-; 16
+        (else 25)) 16)
+(test "Addition and If" (+ 2 (if (> b a) b a)) 6)
 
-(+ 2 (if (> b a) b a))
-; 6
-
-(* (cond ((> a b) a) ; 3 > 4
+(test "Multiply, Cond and Addition" (* (cond ((> a b) a) ; 3 > 4
         ((< a b) b) ; 3 < 4
         (else -1)) 
     (+ a 1))
-; 4 * (3 + 1) = 16
+16)
 
