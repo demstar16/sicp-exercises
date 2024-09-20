03ecc129efc8a42d8d8c2d45ee7951f3b1060fe2
dempseyt <dempsey.thompson@journeyone.com.au>
2024-09-20 10:35:35 +0800

    Exercise 2.53

--- chapter2/2.53.rkt
+++ chapter2/2.53.rkt
@@ -1,20 +1,15 @@
-(list 'a 'b 'c)
-(a b c) 
+#lang racket
 
-(list (list 'george))
-((george))
+(require "../test-harness.rkt")
 
-(cdr '((x1 x2) (y1 y2)))
-(y1 y2)
+(define (cadr list)
+    (car (cdr list)))
 
-(cadr '((x1 x2) (y1 y2)))
-x2
+(test "(list 'a 'b 'c) -> (a b c)" (list 'a 'b 'c) '(a b c))
+(test "(list (list 'george)) -> ((george))" (list (list 'george)) '((george)))
+(test "(cdr '((x1 x2) (y1 y2))) -> (y1 y2)" (cdr '((x1 x2) (y1 y2))) '((y1 y2)))
+(test "(cadr '((x1 x2) (y1 y2))) -> x2" (cadr '((x1 x2) (y1 y2))) '(y1 y2)) ; REMEMBER: cdr will always return in a list, it's a pair of what you get and the empty list. So even if it's one item, when using cdr, it will return a list or in this case since it is already a list, a nested list.
+(test "(pair? (car '(a short list))) -> #f" (pair? (car '(a short list))) #f)
+(test "(memq 'red '((red shoes) (blue socks))) -> #f" (memq 'red '((red shoes) (blue socks))) #f)
+(test "(memq 'red '(red shoes blue socks)) -> (red shoes blue socks)" (memq 'red '(red shoes blue socks)) '(red shoes blue socks))
 
-(pair? (car '(a short list)))
-#f
-
-(memq 'red '((red shoes) (blue socks)))
-#f
-
-(memq 'red '(red shoes blue socks))
-(red shoes blue socks)
