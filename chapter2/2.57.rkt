3113b1927a4560e32e3f6fe89e52cbfbd4bc10a1
dempseyt <dempsey.thompson@journeyone.com.au>
2024-09-20 10:35:50 +0800

    Exercise 2.57

---
+++ chapter2/2.57.rkt
@@ -0,0 +1,55 @@
+#lang racket
+
+(require "../test-harness.rkt")
+(require "../utils.rkt")
+
+
+(define (exponentiation? x)
+    (and (pair? x) (eq? (car x) '**)))
+
+(define (new-augend s) 
+    (if (null? (cdddr s))
+        (caddr s)
+        (cons '+ (cddr s))))
+
+(define (new-multiplicand p)
+    (if (null? (cdddr p))
+        (caddr p)
+        (cons '* (cddr p))))
+
+(define (base e) (cadr e))
+(define (exponent e) (caddr e))
+
+(define (make-exponentiation base exponent)
+    (cond ((=number? exponent 0) 1)
+        ((=number? exponent 1) base)
+        ((and (number? base) (number? exponent)) (expt base exponent))
+        (else (list '** base exponent))))
+
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp)
+         (if (same-variable? exp var) 1 0))
+        ((sum? exp)
+         (make-sum (deriv (addend exp) var)
+                   (deriv (new-augend exp) var)))
+        ((product? exp)
+         (make-sum
+           (make-product (multiplier exp)
+                         (deriv (new-multiplicand exp) var))
+           (make-product (deriv (multiplier exp) var)
+                         (new-multiplicand exp))))
+        ((exponentiation? exp)
+         (make-product
+          (make-product (exponent exp)
+                        (make-exponentiation
+                         (base exp)
+                         (make-sum (exponent exp) -1)))
+          (deriv (base exp) var)))
+        (else
+         (error "unknown expression type -- DERIV" exp))))
+
+(test "d/dx(x^2)" (deriv '(** x 2) 'x) '(* 2 x))
+(test "d/dx(ax^2)" (deriv '(* a (** x 2)) 'x) '(* a (* 2 x)))
+(test "d/dx(ax^2 + bx)" (deriv '(+ (* a (** x 2)) (* b x)) 'x) '(+ (* a (* 2 x)) b))
+(test "d/dx(ax^2 + bx + c)" (deriv '(+ (+ (* a (** x 2)) (* b x)) c) 'x) '(+ (* a (* 2 x)) b))
